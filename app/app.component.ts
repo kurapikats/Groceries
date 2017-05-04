99480588156742203ee0b8f18bb2acec1f69c4cc
kurapikats <kurapikats@yaho.com>
2017-05-04 22:19:55 +0800

    add basic two-way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,14 +5,25 @@ import { Component } from "@angular/core";
   template: `
     <StackLayout>
       <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
-      <TextField hint="Email Address" keyboardType="email"
+      <TextField hint="Email Address" keyboardType="email" [(ngModel)]="email"
         autocorrect="false" autocapitalizationType="none"></TextField>
       <TextField hint="Password" secure="true"></TextField>
 
-      <Button text="Sign in" class="submit-button"></Button>
-      <Button text="Sign up for Groceries"></Button>
+      <Button [text]="isLoggingIn ? 'Sign in' : 'Sign up'" class="submit-button" (tap)="submit()"></Button>
+      <Button [text]="isLoggingIn ? 'Sign up' : 'Back to login'" (tap)="toggleDisplay()"></Button>
     </StackLayout>
   `,
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  email = "jesus.nana@gmail.com";
+  isLoggingIn = true;
+
+
+  submit() {
+    alert("You're using: " + this.email);
+  }
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
